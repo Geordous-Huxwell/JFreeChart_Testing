5afcb7dc4ee4543e416812f6e49622f9b6f00d41
Geordous-Huxwell <jconl266@mtroyal.ca>
2022-03-04 17:30:53 -0700

    test18

--- test/DataUtilitiesTest.java
+++ test/DataUtilitiesTest.java
@@ -246,7 +246,7 @@ class DataUtilitiesTest {
         	actualValues[i] = (double) actual.getValue(i);
         }
         
-//        assertArrayEquals(expectedValues,actualValues);
+        assertArrayEquals(expectedValues,actualValues);
         
     }
 	
